7edcdd5ca530ac86c96fc804833cd459e5102331
aiden cox <cox.378@wright.edu>
2025-03-06 00:48:18 -0500

    got 2D array working w questions printing out

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 
 int main() {
+    std::string questions[] = {"1. What year was C++ created: ",
+                               "2. Who invented C++?: ",
+                               "3. What is the predecessor of C++?: ",
+                               "4. Is the Earth flat?: ",
+    };
+    std::string options[][4] = {{"A. 1969", "B. 1975", "C. 1985", "D. 1989"},
+{"A. Guido van Rossum", "B. Bjarne Stroustrup", "C. John Carmack", "D. Mark Zuckerburg"},
+{"A. C", "B. C+", "C. C--", "D. B++"},
+{"A. Yes", "B. No", "C. sometimes", "D. what is earth?",}
+};
 
+    char answerKey[] = {'C', 'B', 'A', 'B'};
 
-    return 0;
+    int size = sizeof(questions) / sizeof(questions[0]);
+    char guess;
+    int score;
+
+    for (int i = 0; i < size; i++) {
+        std::cout << "**********************************\n";
+        std::cout << questions[i] << '\n';
+        std::cout << "**********************************\n";
+
+        for (int j = 0; j < sizeof(options[i])/sizeof(options[i][0]); j++) {
+            std::cout << options[i][j] << '\n';
+        }
+    }
+return 0;
+
 }
